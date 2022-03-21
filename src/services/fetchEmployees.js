f0fd9ff56a717f5aa0e53fdb36a8cb4bbe6d99e1
Elencrissy <cristina.furgala@gmail.com>
2022-03-21 11:14:20 +0100

    Fetch synthaxe

--- src/services/fetchEmployees.js
+++ src/services/fetchEmployees.js
@@ -1,8 +1,12 @@
-const fetchEmployees = async () => {
+const fetchEmployees = () => {
     const url = 'http://localhost:3000/employees'
-    const res = await fetch(url)
-    const data = await res.json()
-    return data
+    return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+        console.log(`Fetch problem: ${error}`)
+            throw error
+        }
+    )
 }
 
 export default fetchEmployees
